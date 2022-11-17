f706eb16b295a3b46bc2d8008e4677874066b9fc
molidie <molinerodiego5@gmail.com>
2022-11-17 12:25:46 -0300

    errores

--- Sistema-congreso/src/main/java/ar/edu/unnoba/poo2022/Sistemacongreso/config/AdminSecurityConfig.java
+++ Sistema-congreso/src/main/java/ar/edu/unnoba/poo2022/Sistemacongreso/config/AdminSecurityConfig.java
@@ -12,59 +12,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@Order(1)
 public class AdminSecurityConfig {
-    private AdminServiceImp userDetailsService;
 
-    @Autowired
-    public AdminSecurityConfig(AdminServiceImp userDetailsService){
-        this.userDetailsService = userDetailsService;
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService(){
-        return getUserDetailsService();
-    }
-
-    private UserDetailsService getUserDetailsService() {
-        return null;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain1(HttpSecurity http) throws Exception {
-        http
-                .antMatcher("/administrador/**")
-                .userDetailsService(userDetailsService)
-                .authorizeHttpRequests((requests) -> requests
-                        .anyRequest().hasAuthority("ROLE_ADMIN")
-                )
-                .formLogin((form) -> form
-                        .loginPage("/administrador/login")
-                        .usernameParameter("email")
-                        .loginProcessingUrl("/administrador/login")
-                        .defaultSuccessUrl("/administrador/home")
-                        .permitAll()
-                )
-                .logout((logout) -> logout
-                        .logoutUrl("/administrador/logout")
-                        .logoutSuccessUrl("/admin/login")
-                        .permitAll()
-                );
-        return http.build();
-    }
-
-    public DaoAuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-                authProvider.setUserDetailsService(userDetailsService);
-                authProvider.setPasswordEncoder(passwordEncoder());
-                return authProvider;
-    }
 }
 
 
--- Sistema-congreso/src/main/java/ar/edu/unnoba/poo2022/Sistemacongreso/service/AdminServiceImp.java
+++ Sistema-congreso/src/main/java/ar/edu/unnoba/poo2022/Sistemacongreso/service/AdminServiceImp.java
@@ -1,20 +1,15 @@
 package ar.edu.unnoba.poo2022.Sistemacongreso.service;
 
+import ar.edu.unnoba.poo2022.Sistemacongreso.model.Admin;
+import ar.edu.unnoba.poo2022.Sistemacongreso.model.Usuario;
 import ar.edu.unnoba.poo2022.Sistemacongreso.repository.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-public class AdminServiceImp implements IAdminService, UserDetailsService {
-    @Autowired
-    private AdminRepository repository;
-
-    @Override
-    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-        return (UserDetails)repository.findByUserName(userName);
-    }
-
+public class AdminServiceImp {
 
 
 }
