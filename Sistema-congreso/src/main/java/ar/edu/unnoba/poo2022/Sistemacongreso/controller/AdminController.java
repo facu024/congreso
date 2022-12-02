9a5c322a80deaf8c308c66614e01d46d2850231c
facu024 <facundoricagno@gmail.com>
2022-12-02 00:22:44 -0300

    Revert "Revert "Revert "xx"""
    This reverts commit d642ace28787a7dd288f8f2f01255a89304e9558.

--- Sistema-congreso/src/main/java/ar/edu/unnoba/poo2022/Sistemacongreso/controller/AdminController.java
+++ Sistema-congreso/src/main/java/ar/edu/unnoba/poo2022/Sistemacongreso/controller/AdminController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Controller
